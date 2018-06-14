cfdd85ce4a7dfb7e37d8d4fa427cdfcb38f3b794
Nicolas Taffoureau <37969996+ntaff@users.noreply.github.com>
2018-06-14 13:39:26 -0400

    Update ExcelReaderExceptions.java

--- src/ExcelReaderExceptions.java
+++ src/ExcelReaderExceptions.java
@@ -2,6 +2,11 @@ package ca.uqac.lif.cep.excelReader;
 
 public class ExcelReaderExceptions extends RuntimeException {
 
+    /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
     public ExcelReaderExceptions(String message) {
         super(message);
     }
